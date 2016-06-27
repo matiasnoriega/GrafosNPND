9cb9a75f023ebff84be1ea35e0592fa81839c42d
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2016-06-27 10:19:26 -0300

    Update Grafo.java

--- Grafo.java
+++ Grafo.java
@@ -60,13 +60,16 @@ public class Grafo{
 	public Arista[] llenarVectorAristas(Integer dimension){
 		Arista[] vectorAristasAuxiliar = new Arista[this.getCantidadAristas()];
 		Integer contador=0;
+		String prueba = "";
 		for(int i=0;i<dimension;i++){
 			for(int j=i+1;j<dimension;j++){
 				vectorAristasAuxiliar[contador] = new Arista(new Vertice(i), new Vertice(j));
+				prueba+=vectorAristasAuxiliar[contador].getOrigen().getNumero()+" "+vectorAristasAuxiliar[contador].getDestino().getNumero()+"\n";
+				System.out.println(contador);
 				contador++;
 			}
 		}
-		
+		System.out.println(prueba);
 		return vectorAristasAuxiliar;
 	}
 
@@ -80,18 +83,16 @@ public class Grafo{
 		Integer[] vectorAuxiliarDesorden = new Integer[dimension];    
 		Integer n =0;      
 		Random r = new Random();
-
+		System.out.println("Dimension "+dimension);
 		//creo un vector de numeros aleatorios del mismo tamaño que el de aristas
 		for(int i=0; i<dimension; i++){
-			n = r.nextInt(dimension+1); 
+			n = r.nextInt(dimension); 
 			vectorAuxiliarDesorden[i] = n; 
-			System.out.println(vectorAuxiliarDesorden[i]);
 		}
 		//ordeno ese vector de numeros aleatorios paralelamente al de aristas
 		for (int i=1; i<dimension; i++){
 			int index = vectorAuxiliarDesorden[i];
 			int j = i;
-
 			while (j > 0 && vectorAuxiliarDesorden[j-1] > index){
 				vectorAuxiliarDesorden[j] = vectorAuxiliarDesorden[j-1];
 				vectorAuxiliarArista[j] = vectorAuxiliarArista[j-1];
@@ -102,10 +103,13 @@ public class Grafo{
 
 		Integer nuevoTamano = acotarCantidadAristas(dimension, this.getPorcentaje());
 		Arista[] vectorTemporal = new Arista[nuevoTamano];
+		String prueba ="";
 		for(int i=0;i<nuevoTamano;i++){
 			vectorTemporal[i]=vectorAuxiliarArista[i];
+			prueba+=vectorTemporal[i].getOrigen().getNumero()+" "+vectorTemporal[i].getDestino().getNumero()+"\n";
 		}
-		
+		System.out.println(prueba);
+		setVectorAristas(vectorTemporal);
 		return vectorTemporal;		
 	}
 
@@ -118,15 +122,139 @@ public class Grafo{
 	public Boolean[][] generarMatrizAdyacencia(Integer dimension, Arista[] vectorAristas){
 		Boolean[][] matrizAuxiliar = generarMatrizAdyacenciaVacia(dimension);
 		Arista[] vectorAristasAuxiliar = vectorAristas;
+		String prueba ="";
 		for(int i=0;i<vectorAristas.length;i++){
 			matrizAuxiliar[vectorAristasAuxiliar[i].getOrigen().getNumero()][vectorAristasAuxiliar[i].getDestino().getNumero()] = true;			
 		}
+		for(int i=0;i<dimension;i++){
+			for(int j=i+1;j<dimension;j++){
+				prueba+=matrizAuxiliar[i][j]+" ";
+			}
+			prueba+="\n";
+		}
+		System.out.println(prueba);
 		
 		return matrizAuxiliar;
 	}
 
 	public Integer acotarCantidadAristas(Integer cantidad, Integer porcentaje){
 		return (cantidad*porcentaje)/100;
+	}
+	
+	//metodos de coloreo
+	
+	public Integer[] coloreoSecuencialAleatorio(){
+		Integer colores[]= new Integer[dimension];
+		boolean iguales;
+		boolean verticeNoColoreado;
+		
+		for (int i = 0 ; i < dimension; i++){
+			//Al principio le asigno el color más  bajo posible
+			// e inicializo las banderas para iniciar la comprobacion 
+			// de vertices adyacentes con el mismo color
+			colores[i]= 1;
+			iguales= false;
+			verticeNoColoreado= true;
+			while (verticeNoColoreado){
+				for (int j = i ; j >=0; j--){
+					// Si son adyacentes compruebo los colores y de acuerdo a eso
+					// cambio las banderas.
+					if (matrizAdyacencia[j][i]){
+						if (colores[i] == colores[j]){
+							iguales=true;
+						}
+					}
+					if (iguales){
+						verticeNoColoreado=true;
+						iguales= false;
+						colores[i]++;
+					}else{
+						verticeNoColoreado=false;
+					}
+					if(colores[i]>this.cantidadColores)
+						this.cantidadColores=colores[i];					
+					
+				}
+			}
+		}
+		this.vectorColores = colores;
+		return colores;
+	}
+	
+	public void coloreoWelshPowell(){
+		int grado[]= new int[dimension];
+		int contador;
+		int aux;
+		int k;
+		boolean fila[]= new boolean[dimension];
+		Grafo grafoAux= this;
+		for (int i=0; i< dimension; i++){
+			contador=0;
+			for (int j=i+1; j < dimension; j++){
+				if (matrizAdyacencia[i][j])
+					contador++;
+			}
+			grado[i]=contador;
+		}
+		for (int i=1; i < dimension; i++){
+			aux=grado[i];
+			for (int l=0; l< dimension; l++)
+				fila[i]=grafoAux.matrizAdyacencia[i][l];
+			k= i-1;
+			while ((k>=0) && (aux< grado[k])){
+				grado[k + 1]= grado[k];
+				for (int l=0; l< dimension; l++)
+					fila[k+1]=grafoAux.matrizAdyacencia[k][l];
+				k--;
+			}
+			grado[k+1]= aux;
+			for (int l=0; l< dimension; l++)
+				grafoAux.matrizAdyacencia[k+1][l]=fila[i];
+		}
+		this.vectorColores =  grafoAux.coloreoSecuencialAleatorio();
+	}
+	
+	public void coloreoMatula(){
+		int grado[]= new int[dimension];
+		int contador;
+		int aux;
+		int k;
+		boolean fila[]= new boolean[dimension];
+		Grafo grafoAux= this;
+		for (int i=0; i< dimension; i++){
+			contador=0;
+			for (int j=i+1; j < dimension; j++){
+				if (matrizAdyacencia[i][j])
+					contador++;
+			}
+			grado[i]=contador;
+		}
+		for (int i=1; i < dimension; i++){
+			aux=grado[i];
+			for (int l=0; l< dimension; l++)
+				fila[i]=grafoAux.matrizAdyacencia[i][l];
+			k= i-1;
+			while ((k>=0) && (aux> grado[k])){
+				grado[k + 1]= grado[k];
+				for (int l=0; l< dimension; l++)
+					fila[k+1]=grafoAux.matrizAdyacencia[k][l];
+				k--;
+			}
+			grado[k+1]= aux;
+			for (int l=0; l< dimension; l++)
+				grafoAux.matrizAdyacencia[k+1][l]=fila[i];
+		}
+		this.vectorColores = grafoAux.coloreoSecuencialAleatorio();
+	}
+	
+	
+	public String toString(){
+		String string = this.dimension+" "+this.cantidadColores+" "+getCantidadAristas()+" "+this.porcentaje+"\n";
+		for(int i =0; i<this.getDimension();i++){
+			string += "Nodo: "+i+" Color: "+this.vectorColores[i]+"\n";
+			
+		}
+		return string;
 	}
 
 
